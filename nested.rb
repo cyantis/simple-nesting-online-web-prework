4147553e41d554cd70a615dcf54a855563160e5c
Cody Yantis <cyantis@gmail.com>
2019-01-29 02:31:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,12 +56,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -77,8 +71,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+	programmer_hash[:yukihiro_matsumoto] = {
+		:known_for => "Ruby",
+	 :languages => ["LISP", "C"]
+	}
 end
 
 def changing_alan
